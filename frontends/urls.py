92f7c254479c1cd061008dd058d9a6c68db475b0
wonjw3638 <wonjw3638@gmail.com>
2022-11-16 17:50:57 +0900

    Make FE

---
+++ frontends/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+app_name = 'frontends'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('choice/', views.choice, name='choice'),
+    path('trailer/', views.trailer, name='trailer'),
+]
---
+++ frontends/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'frontends/index.html')
+
+def choice(request):
+    return render(request, 'frontends/choice.html')
+
+def trailer(request):
+    return render(request, 'frontends/trailer.html')
