8c1b7984b614f59632b905dddeef4df615296657
wonjw3638 <wonjw3638@gmail.com>
2022-11-20 23:02:50 +0900

    update detail

--- frontends/views.py
+++ frontends/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+
 
 def index(request):
     return render(request, 'frontends/index.html')
@@ -14,3 +15,20 @@ def result(request):
 
 def detail(request):
     return render(request, 'frontends/detail.html') 
+
+# # @require_POST
+# def create_comment(request, review_pk):
+#     review = get_object_or_404(Review, pk=review_pk)
+#     comment_form = CommentForm(request.POST)
+#     if comment_form.is_valid():
+#         comment = comment_form.save(commit=False)
+#         comment.review = review
+#         comment.user = request.user
+#         comment.save()
+#         return redirect('community:detail', review.pk)
+#     context = {
+#         'comment_form': comment_form,
+#         'review': review,
+#         'comments': review.comment_set.all(),
+#     }
+#     return render(request, 'community/detail.html', context)
